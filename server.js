81d78c488bd395f46b805fdb38ade8a36ae72b49
MC Naveen <funmemcn@gmail.com>
2020-09-27 00:38:29 +0530

    Added Pronunce

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var cheerio = require('cheerio');
 var request = require('request');
+var nlp = require('compromise')
+var nlpPronounce = require('compromise-pronounce')
+nlp.extend(nlpPronounce)
 const randomUseragent = require('random-useragent');
 var rua = randomUseragent.getRandom();
 var app = express();
@@ -33,9 +36,13 @@ app.get('/', function (req, res) {
       var post = $('.section #shared_section');
       var word = post.find('#random_word').eq(0).text().replace('\r\n\t\t\t\t\t', '').replace('\r\n\t\t\t\t', '').replace('\n\t\t\t\t\t','').replace('\n\t\t\t\t','');
       var definition = post.find('#random_word_definition').eq(0).text().replace('\n', '');
-      
+      var pronounceword = word;
+      var doc = nlp (pronounceword);
+      var pronounces = doc.terms().pronounce().map(o=> o.pronounce).toString();
+      var pronounce = pronounces.replace(",", "");
+
       // create an object
-      wordOfDay.push({word: word.charAt(0).toUpperCase() + word.slice(1), definition: definition.charAt(0).toUpperCase() + definition.slice(1)})
+      wordOfDay.push({word: word.charAt(0).toUpperCase() + word.slice(1), definition: definition.charAt(0).toUpperCase() + definition.slice(1), pronunciation: pronounce.charAt(0).toUpperCase() + pronounce.slice(1)})
 
       console.log("User-Agent:", rua);
 
