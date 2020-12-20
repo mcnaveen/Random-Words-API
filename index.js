e420b122c3fef42ed99ddb11d97b388a5a36b250
Santhosh Veer <veerfun23@gmail.com>
2020-12-20 09:51:31 +0530

    Security headers

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.get('/', function (req, res) {
   res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
   res.header('Access-Control-Allow-Methods', 'GET');
   res.header('X-Frame-Options', 'DENY');
+  res.header('X-XSS-Protection', '1; mode=block');
   res.header('X-Content-Type-Options', 'nosniff');
   res.header('Strict-Transport-Security', 'max-age=63072000');
   res.setHeader('Content-Type', 'application/json');
