d018ce6cfafd0cfa81cbfcff9848900119c0f437
MC Naveen <funmemcn@gmail.com>
2020-12-28 21:53:32 +0530

    :bug: Removed Proxy

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ var app = express();
 var wordOfDay = [];
 
 // Load the Main JS
+
+app.get('/', function (req,res) {
+  res.send('Endpoint Missing');
+});
+
 app.get('/word', function (req, res) {
   // allow access from other domains
   res.header('Access-Control-Allow-Origin', '*');
@@ -21,74 +26,44 @@ app.get('/word', function (req, res) {
   res.header('X-Content-Type-Options', 'nosniff');
   res.header('Strict-Transport-Security', 'max-age=63072000');
   res.setHeader('Content-Type', 'application/json');
-  app.disable( 'x-powered-by' );
-  
+  app.disable('x-powered-by');
+
   // use Cheerio to make request
   request({
     method: 'GET',
     url: 'https://randomword.com/',
-    proxy: proxyGenerator(),
     headers: {
-        'User-Agent': rua // optional headers
-     }
-  }, function(err, response, body, callback) {
-      if (err) return console.error(err);
-      
-      // get the HTML body from WordThink.com
-      $ = cheerio.load(body);
+      'User-Agent': rua // optional headers
+    }
+  }, function (err, response, body, callback) {
+    if (err) return console.error(err);
 
-      if(wordOfDay.length > 0){
-        wordOfDay = [];
-      }
+    // get the HTML body from WordThink.com
+    $ = cheerio.load(body);
 
-      var post = $('.section #shared_section');
-      var word = post.find('#random_word').eq(0).text().replace('\r\n\t\t\t\t\t', '').replace('\r\n\t\t\t\t', '').replace('\n\t\t\t\t\t','').replace('\n\t\t\t\t','');
-      var definition = post.find('#random_word_definition').eq(0).text().replace('\n', '');
-      var pronounceword = word;
-      var doc = nlp (pronounceword);
-      var pronounces = doc.terms().pronounce().map(o=> o.pronounce).toString();
-      var pronounce = pronounces.replace(",", "");
+    if (wordOfDay.length > 0) {
+      wordOfDay = [];
+    }
 
-      // create an object
-      wordOfDay.push({word: decodeURI(word.charAt(0).toUpperCase() + word.slice(1)), definition: decodeURI(definition.charAt(0).toUpperCase() + definition.slice(1)), pronunciation: decodeURI(pronounce.charAt(0).toUpperCase() + pronounce.slice(1))})
+    var post = $('.section #shared_section');
+    var word = post.find('#random_word').eq(0).text().replace('\r\n\t\t\t\t\t', '').replace('\r\n\t\t\t\t', '').replace('\n\t\t\t\t\t', '').replace('\n\t\t\t\t', '');
+    var definition = post.find('#random_word_definition').eq(0).text().replace('\n', '');
+    var pronounceword = word;
+    var doc = nlp(pronounceword);
+    var pronounces = doc.terms().pronounce().map(o => o.pronounce).toString();
+    var pronounce = pronounces.replace(",", "");
 
-      console.log("User-Agent:", rua);
-        
-      // return a JSON object as a response
-     res.send(JSON.stringify(wordOfDay, null, 4));
+    // create an object
+    wordOfDay.push({ word: decodeURI(word.charAt(0).toUpperCase() + word.slice(1)), definition: decodeURI(definition.charAt(0).toUpperCase() + definition.slice(1)), pronunciation: decodeURI(pronounce.charAt(0).toUpperCase() + pronounce.slice(1)) })
+
+    console.log("User-Agent:", rua);
+
+    // return a JSON object as a response
+    res.send(JSON.stringify(wordOfDay, null, 4));
 
   });
 
 });
-
-// Random Proxy
-function proxyGenerator() {
-  let ip_addresses = [];
-  let port_numbers = [];
-  let proxy;
-
-  request("https://sslproxies.org/", function(error, response, html) {
-    if (!error && response.statusCode == 200) {
-      const $ = cheerio.load(html);
-
-      $("td:nth-child(1)").each(function(index, value) {
-        ip_addresses[index] = $(this).text();
-      });
-
-      $("td:nth-child(2)").each(function(index, value) {
-        port_numbers[index] = $(this).text();
-      });
-    } else {
-      console.log("Error loading proxy, please try again");
-    }
-
-    ip_addresses.join(", ");
-    port_numbers.join(", ");
-
-    //console.log("IP Addresses:", ip_addresses);
-    //console.log("Port Numbers:", port_numbers);
-  });
-}
 
 // start app on localhost port 3000
 var port = process.env.PORT || 3000;
