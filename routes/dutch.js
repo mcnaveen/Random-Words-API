941b3ebcee829a63408ec5327426dd92e0852c38
mcnaveen <funmemcn@gmail.com>
2022-03-04 23:39:04 +0530

    :bug: Updated Browser Agent

--- routes/dutch.js
+++ routes/dutch.js
@@ -26,7 +26,7 @@ router.get("/", function (req, res) {
     method: "GET",
     url: "https://www.generatormix.com/random-dutch-words-generator",
     headers: {
-      "User-Agent": rua,
+      "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
     },
   })
     .then(function (response) {
