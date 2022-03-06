4ac836d807a9426b2c99ed877073b7ddefe963bb
mcnaveen <funmemcn@gmail.com>
2022-03-06 13:06:14 +0530

    :sparkles: Random Dutch Words

--- routes/dutch.js
+++ routes/dutch.js
@@ -1,12 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const axios = require("axios");
-const cheerio = require("cheerio");
 const { pronounce } = require("node-pronounce");
-const randomUseragent = require("random-useragent");
-const rua = randomUseragent.getRandom();
-const { randNum } = require("../utils/randomNum");
-const rand = randNum(1, 8);
+const dutchWordsData = require("../data/dutch/words.json"); //Import Dutchs
+const { randomArrayItem } = require("../utils/randomArrayItem");
 var dutchRandomWord = [];
 
 router.get("/", function (req, res) {
@@ -21,41 +17,27 @@ router.get("/", function (req, res) {
   res.header("X-Content-Type-Options", "nosniff");
   res.header("Strict-Transport-Security", "max-age=63072000");
   res.setHeader("Content-Type", "application/json");
+  
+  if (dutchRandomWord.length > 0) {
+    dutchRandomWord = [];
+  }
 
-  axios({
-    method: "GET",
-    url: "https://www.generatormix.com/random-dutch-words-generator",
-    headers: {
-      "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
-    },
-  })
-    .then(function (response) {
-      $ = cheerio.load(response.data);
+  const fetchRandomWord = randomArrayItem(dutchWordsData);
 
-      if (dutchRandomWord.length > 0) {
-        dutchRandomWord = [];
-      }
-      let post = $("#output > div");
-      let word = post.find(`div:nth-child(${rand}) > h3`)[0].children[0].data;
-      let definition = post
-        .find(`div:nth-child(${rand}) > span`)[0]
-        .children[0].data.replace("Meaning: ", "");
-      let pronunciation = pronounce(word);
-      dutchRandomWord.push({
-        word: decodeURI(word.charAt(0).toUpperCase() + word.slice(1)),
-        definition: decodeURI(
-          definition.charAt(0).toUpperCase() + definition.slice(1)
-        ),
-        pronunciation: decodeURI(
-          pronunciation.charAt(0).toUpperCase() + pronunciation.slice(1)
-        ),
-      });
-      console.log(dutchRandomWord);
-      res.send(JSON.stringify(dutchRandomWord, null, 2));
-    })
-    .catch((error) => {
-      console.log(error.response);
-    });
+  const word = fetchRandomWord.word;
+  const definition = fetchRandomWord.definition;
+  const pronunciation = pronounce(word);
+
+  dutchRandomWord.push({
+    word: decodeURI(word.charAt(0).toUpperCase() + word.slice(1)),
+    definition: decodeURI(
+      definition.charAt(0).toUpperCase() + definition.slice(1)
+    ),
+    pronunciation: decodeURI(
+      pronunciation.charAt(0).toUpperCase() + pronunciation.slice(1)
+    ),
+  });
+  res.send(JSON.stringify(dutchRandomWord, null, 2));
 });
 
 module.exports = router;
---
+++ utils/randomArrayItem.js
@@ -0,0 +1,5 @@
+function randomArrayItem(items) {
+  return items[Math.floor(Math.random() * items.length)];
+}
+
+module.exports.randomArrayItem = randomArrayItem;
