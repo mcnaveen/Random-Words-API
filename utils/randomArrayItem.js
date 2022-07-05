245d16a5baefbd19a647c411a176be45ae4caef0
mcnaveen <funmemcn@gmail.com>
2022-07-05 19:07:17 +0530

    refactor(general): ♻️  add random-array-item package and few improvements

--- utils/randomArrayItem.js
+++
@@ -1,6 +0,0 @@
-function randomArrayItem(items) {
-  return items[Math.floor(Math.random() * items.length)];
-}
-
-const _randomArrayItem = randomArrayItem;
-export { _randomArrayItem as randomArrayItem };
