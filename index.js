74e8642bbc021564a4a0fcb52a2502a439acf08b
Diogo Goncalves <goncalvesdiogo99@gmail.com>
2022-10-01 21:28:02 +1000

    refactor: replace var with const + add line break

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use((err, req, res, next) => {
     });
 });
 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, function() {
     console.log('listening on port ' + port);
 });
