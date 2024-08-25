25608bdac9f0b0ba7a8a7638700db24e796e5fee
Naveen MC <8493007+mcnaveen@users.noreply.github.com>
2024-08-25 20:18:26 +0530

    fix: swagger ui scripts not loading

--- swagger-ui.js
+++ swagger-ui.js
@@ -1,10 +1,17 @@
-import express from 'express';
-import swaggerUi from 'swagger-ui-express';
-import specs from './swagger.js';
+import express from "express";
+import swaggerUi from "swagger-ui-express";
+import specs from "./swagger.js";
 
 const router = express.Router();
 
-router.use('/', swaggerUi.serve);
-router.get('/', swaggerUi.setup(specs, { explorer: true }));
+router.use("/", swaggerUi.serve);
+router.get(
+  "/",
+  swaggerUi.setup(specs, {
+    explorer: true,
+    customJs: ["swagger-ui-bundle.js", "swagger-ui-standalone-preset.js"],
+    customCss: "swagger-ui.min.css",
+  })
+);
 
 export default router;
