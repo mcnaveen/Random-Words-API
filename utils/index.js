039ad1b226b7c31d6dccf88b1c3cd4072c0bb850
Naveen MC <8493007+mcnaveen@users.noreply.github.com>
2024-08-25 19:52:21 +0530

    bump rate limit

--- utils/index.js
+++ utils/index.js
@@ -8,7 +8,7 @@ const rua = randomUseragent.getRandom();
 
 export const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // Limit each IP to 100 requests per windowMs
+  max: 30, // Limit each IP to 100 requests per windowMs
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   message: {
